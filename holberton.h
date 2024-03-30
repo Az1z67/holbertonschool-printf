734ea2cd0310b9dc7d104b4be61cbd8f22ea225f
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-03-30 12:47:45 +0000

    handler, lib

--- holberton.h
+++ holberton.h
@@ -10,4 +10,16 @@ int _printf(const char *, ...);
 int _putchar(char);
 int buffer(char);
 
+int _strlen(const char *);
+int print(char *);
+
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+typedef struct _format
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
 #endif

c7c292821f11c901f4abc093222e2a57ba377166
Az1z67 <abbasovaziz61@gmail.com>
2024-03-31 17:39:33 +0400

    print_binary

--- holberton.h
+++ holberton.h
@@ -16,6 +16,19 @@ int print(char *);
 int handler(const char *, va_list);
 int percent_handler(const char *, va_list, int *);
 
+
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
 typedef struct _format
 {
 	char type;
